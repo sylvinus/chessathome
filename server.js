d511c63e8dbfb72322d562053f695bf6e1e09fd3
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2011-09-28 10:31:33 +0200

    fix joyent worker

--- server.js
+++ server.js
@@ -11,10 +11,22 @@ app.get("/",function(req,res) {
   res.redirect("http://chessathome.org/");
 });
 
-var CLI_PROCESSES=4;
+var ENGINE_PATH = './build/engine.js'
+  , socketio = require('socket.io-client')
+  , path = require('path')
+  , Worker = require('webworker').Worker
+  , startWorker = require("./worker/worker.js").startWorker;
 
-for (var i=0;i<CLI_PROCESSES;i++) {
-  require("./worker/client.js").start({'host':'joshfire.nko2.nodeknockout.com','port':8000, reconnect: 5000}, 'joyent-' + i);
+//TODO CLI switches
+var CLI_PROCESSES = 4;
+
+var VERBOSE = false;
+
+if (require.main === module) {
+  for (var i=0;i<CLI_PROCESSES;i++) {
+    startWorker({host: process.ARGV[2] || 'chessathome.org', port: process.ARGV[3] || 80}, 'joyent-' + i,path.resolve(__dirname, ENGINE_PATH), socketio, Worker, true);
+  }
 }
 
-app.listen(process.env.PORT || 3000);
+//TODO stop ?
+app.listen(process.env.PORT || 3001);
