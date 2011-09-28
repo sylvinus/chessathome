f478d2ca2256ee76b29e5b3ba2b7eeaba4b88c30
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2011-09-28 10:55:38 +0200

    fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var express = require('express')
 app.get("/",function(req,res) {
   res.redirect("http://chessathome.org/");
 });
-
+/*
 var ENGINE_PATH = './build/engine.js'
   , socketio = require('socket.io-client')
   , path = require('path')
@@ -27,6 +27,6 @@ if (require.main === module) {
     startWorker({host: process.ARGV[2] || 'chessathome.org', port: process.ARGV[3] || 80}, 'joyent-' + i,path.resolve(__dirname, ENGINE_PATH), socketio, Worker, true);
   }
 }
-
+*/
 //TODO stop ?
 app.listen(process.env.PORT || 3001);
